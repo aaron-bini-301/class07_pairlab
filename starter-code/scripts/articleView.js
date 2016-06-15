e72f4e5b03276da29d46184c258571de93428d08
Aaron Bini <bini.aaron@gmail.com>
2016-06-15 08:16:47 -0700

    making progress

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -114,8 +114,8 @@
   };
 
   articleView.initAdminPage = function() {
-    // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
-    var template; // = ...?
+    // DONE: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
+    var template = Handlebars.compile($('#author-template').text());
 
     // DONE: We use `forEach` here because we are relying on the side-effects of the callback function:
     // appending to the DOM.
